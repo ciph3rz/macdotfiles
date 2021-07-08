f15181666ec1a7710beaced76d377411941c807f
ciph3rz <ciph3rz@zoho.com>
2021-07-08 18:47:38 -0400

    Added echo cmds and deactived offensive docker cln

--- clone.sh
+++ clone.sh
@@ -14,16 +14,19 @@ ZPATH=$HOME/.oh-my-zsh/custom
 # Personal
 #git clone git@github.com:driesvints/driesvints.com.git $SITES/driesvints.com
 # zsh themes
+echo "Cloning zsh repos"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZPATH/themes/powerlevel10k
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZPATH/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git $ZPATH/plugins/zsh-syntax-highlighting
 # Cloning Powerline fonts and installing
+echo "cloning powerline fonts"
 git clone https://github.com/powerline/fonts.git --depth=1 ~/code/fonts
 cd fonts
 ./install.sh
 
+echo "cloning Doom Emacs"
 git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
 echo "Installing Doom Emacs"
 ~/.emacs.d/bin/doom install
 
-git clone --depth 1 https://github.com/ciph3rz/offensive-docker ~/code/
+# git clone --depth 1 https://github.com/ciph3rz/offensive-docker ~/code/
